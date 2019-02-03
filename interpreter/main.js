19ce0423cb8ee0a30927fcb6b78b26ce2e98b63c
Fernando Pelliccioni <fpelliccioni@gmail.com>
2019-02-03 06:23:17 -0300

    improvements

--- interpreter/main.js
+++ interpreter/main.js
@@ -11,9 +11,35 @@ function Iterator(data, index, name) {
     this.name = name;
 }
 
+
+function resetState() {
+    
+    var hg_right_x_a = document.getElementById('hg-right-x-a');
+    hg_right_x_a.innerHTML = '';
+
+
+    lines = [];
+    prevLine = "";
+    iterators_int = {};
+    iterators_gui = {};
+    predicates = [];
+    sequences = {};
+
+    stats_it_moves = 0;
+    stats_it_cmps = 0;
+    stats_pred_appls = 0;
+    stats_swaps = 0;
+    stats_assigments = 0;    
+}
+
+function resetStats() {
+    var hg_right_x_b = document.getElementById('hg-right-x-b');
+    hg_right_x_b.innerHTML = '';
+}
+
 function updateState() {
-    var MEW = document.getElementById('MEW');
-    MEW.innerHTML = '';
+    var hg_right_x_a = document.getElementById('hg-right-x-a');
+    hg_right_x_a.innerHTML = '';
 
     for (var key in iterators_int) {
         // console.log(iterators_int[key]);
@@ -26,33 +52,73 @@ function updateState() {
         var length = data.length;
         var text = '<p id="Status">source(<b>' + key + '</b>) = ' + data[it.index] + '</p>';
 
-        MEW.innerHTML += text;
+        hg_right_x_a.innerHTML += text;
     }
 }
 
 function updateStats() {
-    var MEE = document.getElementById('MEE');
-    MEE.innerHTML = '';
+    var hg_right_x_b = document.getElementById('hg-right-x-b');
+    hg_right_x_b.innerHTML = '';
 
-    MEE.innerHTML += '<p id="Status"><b>Iterator movements</b>: ' + stats_it_moves+ '</p>';
-    MEE.innerHTML += '<p id="Status"><b>Iterator comparisons</b>: ' + stats_it_cmps+ '</p>';
-    MEE.innerHTML += '<p id="Status"><b>Predicate applications</b>: ' + stats_pred_appls+ '</p>';
-    MEE.innerHTML += '<p id="Status"><b>Swaps</b>: ' + stats_swaps+ '</p>';
-    MEE.innerHTML += '<p id="Status"><b>Assignments</b>: ' + stats_assigments+ '</p>';
+    hg_right_x_b.innerHTML += '<p id="Status"><b>Iterator displacements</b>: ' + stats_it_moves+ '</p>';
+    hg_right_x_b.innerHTML += '<p id="Status"><b>Iterator comparisons</b>:   ' + stats_it_cmps+ '</p>';
+    hg_right_x_b.innerHTML += '<p id="Status"><b>Pred/Rel applications</b>:  ' + stats_pred_appls+ '</p>';
+    hg_right_x_b.innerHTML += '<p id="Status"><b>Swaps</b>:                  ' + stats_swaps+ '</p>';
+    hg_right_x_b.innerHTML += '<p id="Status"><b>Assignments</b>:            ' + stats_assigments+ '</p>';
 }
+
+function addLog(text) {
+    var hg_right_b_b = document.getElementById('hg-right-b-b');
+    // hg_right_b_b.innerHTML = '';
+    hg_right_b_b.innerHTML += '<p id="Status">' + text+ '</p>';
+}
+
+function addLogEqual(a, b, res) {
+    addLog('equal(' + a.data.name + '<sub>' + a.index + '</sub>, ' + b.data.name + '<sub>' + b.index + '</sub>) = ' + res);
+}
+
+function addLogSuccessor(it, res_it) {
+    addLog('successor(' + it.data.name + '<sub>' + it.index + '</sub>) = ' + res_it.data.name + '<sub>' + res_it.index + '</sub>');
+}
+
+function addLogPredecessor(it, res_it) {
+    // console.log(it)
+    // console.log(res_it)
+    addLog('predecessor(' + it.data.name + '<sub>' + it.index + '</sub>) = ' + res_it.data.name + '<sub>' + res_it.index + '</sub>');
+}
+
+function addLogSource(it, res) {
+    addLog('source(' + it.data.name + '<sub>' + it.index + '</sub>) = ' + res);
+}
+
+function addLogSink(it, x) {
+    addLog('source(' + it.data.name + '<sub>' + it.index + '</sub>, ' + x + ')');
+}
+
+function addLogPredicate(name, x, res) {
+    addLog(name + '(' + x + ') = ' + res);
+}
+
+
 
 function updateStatus() {
     updateState();
     updateStats();
 }
 
-function initAlert(interpreter, scope) {
+function resetStatus() {
+    resetState();
+    resetStats();
+}
+
+
+function initFunctions(interpreter, scope) {
     var alert_wrapper = function(text) {
         return alert(arguments.length ? text : '');
     };
 
     var error = function(text) {
-        var msg = '<p><span style="color:red">ERROR: </span>' + text + '</p>';
+        var msg = '<p id="OutputMsg"><span style="color:red">ERROR: </span>' + text + '</p>';
 
         var output = document.getElementById('output');
         output.innerHTML += msg;
@@ -63,7 +129,7 @@ function initAlert(interpreter, scope) {
     }
 
     var print_wrapper = function(text) {
-        var msg = '<p><span style="color:cyan">INFO: </span>' + text + '</p>';
+        var msg = '<p id="OutputMsg"><span style="color:cyan">INFO: </span>' + text + '</p>';
 
         var output = document.getElementById('output');
         output.innerHTML += msg;
@@ -76,9 +142,7 @@ function initAlert(interpreter, scope) {
 
     var assign_it_wrapper = function(target, source) {
 
-        // console.log(target)
-        // console.log(source)
-        // console.log(iterators_gui[target.name])
+        var elements = target.data.elements;
 
         target = new Iterator(target.data, source.index, target.name);
         iterators_int[target.name] = target;
@@ -95,60 +159,68 @@ function initAlert(interpreter, scope) {
     };
 
 
-    var successor_wrapper = function(it, move = true) {
-        var data = it.data.data;
+    var successor_wrapper = function(it_par, move = true) {
+        var data = it_par.data.data;
         var max = data.length;
 
-        // console.log(it.index)
-        if (it.index >= max) {
+        // console.log(it_par.index)
+        if (it_par.index >= max) {
             error('out of range');
             disable('disabled');
             return;
         }
 
         if (move) {
-            moveIteratorTo(two, iterators_gui[it.name], it.data.elements[it.index + 1])
+            moveIteratorTo(two, iterators_gui[it_par.name], it_par.data.elements[it_par.index + 1])
         }
-        var it = new Iterator(it.data, it.index + 1, it.name);
+        var it = new Iterator(it_par.data, it_par.index + 1, it_par.name);
         iterators_int[it.name] = it;
 
         ++stats_it_moves;
 
         updateStatus();
+        addLogSuccessor(it_par, it)
         return it;
     };
 
-    var predecessor_wrapper = function(it, move = true) {
-        // console.log(it.index)
-        if (it.index <= 0) {
+    var predecessor_wrapper = function(it_par, move = true) {
+        // console.log(it_par.index)
+        if (it_par.index <= 0) {
             error('out of range');
             disable('disabled');
             return;
         }
 
         if (move) {
-            moveIteratorTo(two, iterators_gui[it.name], it.data.elements[it.index - 1])
+            moveIteratorTo(two, iterators_gui[it_par.name], it_par.data.elements[it_par.index - 1])
         }
-        var it = new Iterator(it.data, it.index - 1, it.name);
+        var it = new Iterator(it_par.data, it_par.index - 1, it_par.name);
         iterators_int[it.name] = it;
         ++stats_it_moves;
 
         updateStatus();
 
+        addLogPredecessor(it_par, it)
         return it;
     };
-
     
-    // var begin_wrapper = function(arr, name, color = '#000075') {
-    // var begin_wrapper = function(arr, name, color = '#99ff99') {
     var begin_wrapper = function(arr, name, color) {
-        console.log(arr)
+        // console.log(arr)
+
+        if (iterators_int[name] != undefined) {
+            var gui = iterators_gui[name];
+            // console.log(gui)
+            color = gui.children[0].fill;
+            // console.log(color)
+            remove_it_wrapper(iterators_int[name]);
+        }
 
         if ( ! color) {
-            console.log(Object.keys(iterators_int).length)
+            // console.log(Object.keys(iterators_int).length)
             color = iterators_colors[Object.keys(iterators_int).length];
         }
-        
+
+
         var index = 0
         var it = new Iterator(arr, index, name);
         var it_gui = drawIterator(two, arr.elements[index], name, color);
@@ -161,12 +233,18 @@ function initAlert(interpreter, scope) {
         return it;
     };
 
-    // var end_wrapper = function(arr, name, color = '#000075') {
-    // var end_wrapper = function(arr, name, color = '#99ff99') {
     var end_wrapper = function(arr, name, color) {
 
+        if (iterators_int[name] != undefined) {
+            var gui = iterators_gui[name];
+            // console.log(gui)
+            color = gui.children[0].fill;
+            // console.log(color)
+            remove_it_wrapper(iterators_int[name]);
+        }
+
         if ( ! color) {
-            console.log(Object.keys(iterators_int).length)
+            // console.log(Object.keys(iterators_int).length)
             color = iterators_colors[Object.keys(iterators_int).length];
         }
 
@@ -188,11 +266,13 @@ function initAlert(interpreter, scope) {
         var max = data.length;
         if (it.index >= max) {
             error('not valid iterator to take the source.');
-
             disable('disabled');
             return;
         }
+        
+
         var s = data[it.index];
+        addLogSource(it, s)
 
         //TODO
         // ++stats_pred_appls;
@@ -201,15 +281,45 @@ function initAlert(interpreter, scope) {
         return s;
     };
 
+    var sink_wrapper = function(it, x) {
+        var data = it.data.data;
+        var elements = it.data.elements;
+
+        var max = data.length;
+        if (it.index >= max) {
+            error('not valid iterator to take the source.');
+            disable('disabled');
+            return;
+        }
+
+        addLogSink(it, x)
+        
+        data[it.index] = x;
+        elements[it.index].group.children[1].value = x;
+
+        ++stats_assigments;
+        updateStatus();
+        two.update();
+    };
+
     var equal_wrapper = function(a, b) {
+        // addLog('equal(' + a.index + ', ' + b.index + ')');
+        // addLog('equal(' + a.data.name + ', ' + b.index + ')');
         ++stats_it_cmps;
         updateStatus();
-        return a.index == b.index;
+        var res = a.index == b.index;
+        addLogEqual(a, b, res)
+        return res;
     };
 
     
-    // var copy_it_wrapper = function(it, name, color = '#000075') {
-    var copy_it_wrapper = function(it, name, color = '#99ff99') {
+    var copy_it_wrapper = function(it, name, color) {
+
+        if ( ! color) {
+            console.log(Object.keys(iterators_int).length)
+            color = iterators_colors[Object.keys(iterators_int).length];
+        }
+
         var index = it.index
         var it = new Iterator(it.data, it.index, name);
         var it_gui = drawIterator(two, it.data.elements[index], name, color);
@@ -264,6 +374,7 @@ function initAlert(interpreter, scope) {
 
         if (sequences[name] != undefined) {
             error('sequence "' + name + '" already exists.');
+            disable('disabled');
             return null;
         }
 
@@ -305,9 +416,16 @@ function initAlert(interpreter, scope) {
         two.update();
     };    
 
-    var increment_predicate_stats_wrapper = function(p, x) {
+    var call_predicate_internal_wrapper = function(name, x, res) {
         ++stats_pred_appls;
         updateStatus();
+
+        addLogPredicate(name, x, res);
+
+        //TODO
+        var hg_right_x_a = document.getElementById('hg-right-x-a');
+        var text = '<p id="Status">' + name + '(' + x + ') = ' + res + '</p>';
+        hg_right_x_a.innerHTML += text;
     };    
 
     var fill_elem_wrapper = function(i, c) {
@@ -322,21 +440,25 @@ function initAlert(interpreter, scope) {
     interpreter.setProperty(scope, 'begin',          interpreter.createNativeFunction(begin_wrapper));
     interpreter.setProperty(scope, 'end',            interpreter.createNativeFunction(end_wrapper));
     interpreter.setProperty(scope, 'source',         interpreter.createNativeFunction(source_wrapper));
+    interpreter.setProperty(scope, 'sink',           interpreter.createNativeFunction(sink_wrapper));
     interpreter.setProperty(scope, 'equal',          interpreter.createNativeFunction(equal_wrapper));
     interpreter.setProperty(scope, 'copy_it',        interpreter.createNativeFunction(copy_it_wrapper));
     interpreter.setProperty(scope, 'remove_it',      interpreter.createNativeFunction(remove_it_wrapper));
     interpreter.setProperty(scope, 'iter_swap',      interpreter.createNativeFunction(iter_swap_wrapper));
     interpreter.setProperty(scope, 'assign_it',      interpreter.createNativeFunction(assign_it_wrapper));
     interpreter.setProperty(scope, 'add_sequence_internal',   interpreter.createNativeFunction(add_sequence_internal_wrapper));
-    interpreter.setProperty(scope, 'set_predicate',  interpreter.createNativeFunction(set_predicate_wrapper));
+    // interpreter.setProperty(scope, 'set_predicate',  interpreter.createNativeFunction(set_predicate_wrapper));
     interpreter.setProperty(scope, 'fill_elem',      interpreter.createNativeFunction(fill_elem_wrapper));
-    interpreter.setProperty(scope, 'increment_predicate_stats', interpreter.createNativeFunction(increment_predicate_stats_wrapper));
+    // interpreter.setProperty(scope, 'increment_predicate_stats', interpreter.createNativeFunction(increment_predicate_stats_wrapper));
+    interpreter.setProperty(scope, 'call_predicate_internal', interpreter.createNativeFunction(call_predicate_internal_wrapper));
+    
 }
 
-
-
 function callPredCode() {
-    return 'function call_predicate(p, x){increment_predicate_stats(); return p(x);};\n';
+    return 'function call_predicate(p, name, x){var res = p(x); call_predicate_internal(name, x, res); return res;};\n'
+         + 'function predicate(p, name) {return function(x) {return call_predicate(p, name, x);};}\n'
+         + 'function call_relation(r, x, y){var res = r(x, y);increment_predicate_stats();return res;}\n'
+         + 'function relation(r){return function(x, y){return call_relation(r, x, y);};}\n';
 }
 
 function addSequenceCode() {
@@ -406,15 +528,40 @@ function editButton() {
     document.getElementById('startButton').style.display = "inline";
     document.getElementById('stepButton').style.display = "none";
     document.getElementById('editButton').style.display = "none";
+    document.getElementById('restartButton').style.display = "none";
 
     var output = document.getElementById('output');
     output.innerHTML = '';
 
-    var MEW = document.getElementById('MEW');
-    MEW.innerHTML = '';
+    var hg_right_x_a = document.getElementById('hg-right-x-a');
+    console.log(hg_right_x_a.innerHTML);
+    hg_right_x_a.innerHTML = '';
 
     two.clear();
 }
+
+function restartButton() {
+    // document.getElementById('dataCodeArea').style.display = "inline";
+    document.getElementById('codeArea').style.display = "inline";
+    document.getElementById('codeHighlightPre').style.display = "none";
+
+    document.getElementById('startButton').style.display = "inline";
+    document.getElementById('stepButton').style.display = "none";
+    document.getElementById('editButton').style.display = "none";
+    document.getElementById('restartButton').style.display = "none";
+
+    var output = document.getElementById('output');
+    output.innerHTML = '';
+
+    var hg_right_x_a = document.getElementById('hg-right-x-a');
+    hg_right_x_a.innerHTML = '';
+
+    resetStatus();
+    two.clear();
+
+    startButton();
+}
+
 
 function startButton() {
     var codeAll = getAllCode();
@@ -428,14 +575,16 @@ function startButton() {
     document.getElementById('startButton').style.display = "none";
     document.getElementById('stepButton').style.display = "inline";
     document.getElementById('editButton').style.display = "inline";
+    document.getElementById('restartButton').style.display = "inline";
 
     var output = document.getElementById('output');
     output.innerHTML = '';
 
-    var MEW = document.getElementById('MEW');
-    MEW.innerHTML = '';
+    var hg_right_x_a = document.getElementById('hg-right-x-a');
+    hg_right_x_a.innerHTML = '';
 
     two.clear();
+    resetStatus();
 
     var codeHighlight = document.getElementById('codeHighlight');
     codeHighlight.innerHTML = codeView;
@@ -444,8 +593,10 @@ function startButton() {
     var output = document.getElementById('output');
     hljs.highlightBlock(output);
 
-    myInterpreter = new Interpreter(codeAll, initAlert);
+    myInterpreter = new Interpreter(codeAll, initFunctions);
     disable('');
+
+    updateStatus();
 }
 
 function stepButton() {
