94cd41d91fdd2234fb815f374d8cd34806824f00
Fernando Pelliccioni <fpelliccioni@gmail.com>
2019-02-12 16:47:01 -0300

    new algos

--- interpreter/main.js
+++ interpreter/main.js
@@ -586,6 +586,157 @@ if (res) {
 
 
 
+,swap_ranges:
+`function swap_ranges(f0, l0, f1) {
+    while ( ! equal(f0, l0)) {
+        iter_swap(f0, f1);
+        f0 = successor(f0);
+        f1 = successor(f1);
+    }
+    return f1; 
+}
+
+var s1 = add_sequence(random_array(), "s1");
+var s2 = add_sequence(random_array(), "s2");
+
+var r = swap_ranges(begin(s1), end(s1), begin(s2));
+print('...');`
+
+
+,swap_ranges_bounded:
+`function swap_ranges_bounded(f0, l0, f1, l1) {
+    while ( ! equal(f0, l0) && ! equal(f1, l1)) {
+        iter_swap(f0, f1);
+        f0 = successor(f0);
+        f1 = successor(f1);
+    } 
+    return [f0, f1];
+}
+
+var s1 = add_sequence(random_array(), "s1");
+var s2 = add_sequence(random_array(5), "s2");
+
+var r = swap_ranges_bounded(begin(s1), end(s1), begin(s2), end(s2));
+var f0 = r[0];
+var f1 = r[1];
+print('...');`
+
+
+
+,swap_ranges_n:
+`var r0 = range_counted("f0", "n");
+var r1 = range_counted("f1", "n");
+function swap_ranges_n(f0, f1, n) {
+    while (n != 0) {
+        iter_swap(f0, f1);
+        f0 = successor(f0);
+        f1 = successor(f1);
+        --n;
+    }
+    return [f0, f1];
+}
+
+var s1 = add_sequence(random_array(), "s1");
+var s2 = add_sequence(random_array(5), "s2");
+
+var r = swap_ranges_n(begin(s1), begin(s2), 5);
+var f0 = r[0];
+var f1 = r[1];
+print('...');`
+
+
+,reverse_n_forward:
+`skip_debug('half_nonnegative');
+skip_debug('twice');
+skip_debug('swap_ranges_n');
+var r = range_counted("f", "n");
+
+function half_nonnegative(n) {return n >> 1;}
+function twice(n) {return n + n;}
+
+function swap_ranges_n(f0, f1, n) {
+    while (n != 0) {
+        iter_swap(f0, f1);
+        f0 = successor(f0);
+        f1 = successor(f1);
+        --n;
+    }
+    return [f0, f1];
+}
+
+function reverse_n_forward(f, n) {
+    if (n < 2) return successor(f, n);
+    var h = half_nonnegative(n);
+    var n_mod_2 = n - twice(h);
+
+    var m = successor(reverse_n_forward(f, h), n_mod_2);
+    var l = reverse_n_forward(m, h);
+
+    swap_ranges_n(f, m, h);
+    return l;
+}
+
+var s = add_sequence(random_array(), "s1");
+print(s);
+var r = reverse_n_forward(begin(s), size(s));
+print(s);
+print('...');`
+
+
+,reverse_copy:
+`//var r = range_counted("f", "n");
+function reverse_copy(f_i, l_i, f_o) {
+    while ( ! equal(f_i, l_i)) {
+        l_i = predecessor(l_i);
+        sink(f_o, source(l_i));
+        f_o = successor(f_o);
+
+    } 
+    return f_o;
+}
+
+var d = add_sequence(random_array(), "d");
+var b = add_sequence(new Array(size(d)), "b");
+
+var res = reverse_copy(begin(d), end(d), begin(b));
+print(d);
+print(b);`
+
+
+,reverse_n_with_buffer:
+`//var r = range_counted("f", "n");
+
+function copy_n(f_i, n, f_o) {
+    while (n != 0) {
+        sink(f_o, source(f_i));
+        f_i = successor(f_i);
+        f_o = successor(f_o);
+        --n;
+    }
+    return [f_i, f_o];
+}
+
+function reverse_copy(f_i, l_i, f_o) {
+    while ( ! equal(f_i, l_i)) {
+        l_i = predecessor(l_i);
+        sink(f_o, source(l_i));
+        f_o = successor(f_o);
+
+    } 
+    return f_o;
+}
+
+function reverse_n_with_buffer(f_i, n, f_b) {
+    return reverse_copy(f_b, copy_n(f_i, n, f_b)[1], f_i);
+}
+
+var s = add_sequence(random_array(), "s");
+var b = add_sequence(new Array(size(s)), "b");
+print(s);
+var r = reverse_n_with_buffer(begin(s), size(s), begin(b));
+print(s);`
+
+
 };
 
 function getSnippet(snippet) {
@@ -822,6 +973,11 @@ function initFunctions(interpreter, scope) {
 
 
     var print_wrapper = function(text) {
+
+        if (text instanceof Sequence) {
+            text = text.data.join(", ");
+        }
+
         var msg = '<p id="OutputMsg"><span style="color:cyan">INFO: </span>' + text + '</p>';
 
         var output = document.getElementById('hg-right-y');
@@ -1054,26 +1210,18 @@ function initFunctions(interpreter, scope) {
 
 
     var iter_swap_wrapper = function(a, b) {
-        var data = a.data.data;
-        // console.log(a)
-        // console.log(data)
-        // var elements = a.data.elements;
-        var colors = a.data.colors;
+        var data_a = a.data.data;
+        var data_b = b.data.data;
+        var colors_a = a.data.colors;
+        var colors_b = b.data.colors;
 
-        // var tmp_fill = elements[a.index].group.children[0].fill;
-        // elements[a.index].group.children[0].fill = elements[b.index].group.children[0].fill;
-        // elements[b.index].group.children[0].fill = tmp_fill;
-
-        var tmp_color = colors[a.index];
-        colors[a.index] = colors[b.index];
-        colors[b.index] = tmp_color;
+        var tmp_color = colors_a[a.index];
+        colors_a[a.index] = colors_b[b.index];
+        colors_b[b.index] = tmp_color;
 
         var tmp = source_value(a);
-        data[a.index] = source_value(b);
-        // elements[a.index].group.children[1].value = source_value(b);
-
-        data[b.index] = tmp;
-        // elements[b.index].group.children[1].value = tmp;
+        data_a[a.index] = source_value(b);
+        data_b[b.index] = tmp;
 
         if (log_stats_enabled) {
             ++stats_swaps;
