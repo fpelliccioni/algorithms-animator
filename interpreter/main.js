ed227cb6fd64bcbc5e8c7fd79da3dae9cd619c06
Fernando Pelliccioni <fpelliccioni@gmail.com>
2019-02-13 10:07:37 -0300

    new algos

--- interpreter/main.js
+++ interpreter/main.js
@@ -645,6 +645,47 @@ var f1 = r[1];
 print('...');`
 
 
+,reverse_n_indexed:
+`var r = range_counted("f", "n");
+
+function reverse_n_indexed(f, n) {
+    var i = 0;
+    --n;
+    while (i < n) {
+        iter_swap(successor(f, i), successor(f, n)); 
+        ++i;
+        --n;
+    } 
+}
+
+var s = add_sequence(random_array(), "s1");
+print(s);
+reverse_n_indexed(begin(s), size(s));
+print(s);
+print('...');`
+
+
+
+,reverse_bidirectional:
+`var r = range_bounded("f", "l");
+
+function reverse_bidirectional(f, l) {
+
+    while (true) {
+        if (equal(f, l)) return;
+        l = predecessor(l);
+        if (equal(f, l)) return;
+        iter_swap(f, l);
+        f = successor(f);        
+    }
+}
+
+var s = add_sequence(random_array(), "s1");
+print(s);
+reverse_bidirectional(begin(s), end(s));
+print(s);
+print('...');`
+
 ,reverse_n_forward:
 `skip_debug('half_nonnegative');
 skip_debug('twice');
@@ -736,7 +777,99 @@ print(s);
 var r = reverse_n_with_buffer(begin(s), size(s), begin(b));
 print(s);`
 
+,reverse_n_adaptive:
+`skip_debug('half_nonnegative');
+skip_debug('twice');
+skip_debug('swap_ranges_n');
+skip_debug('copy_n');
+skip_debug('reverse_copy');
+skip_debug('reverse_n_with_buffer');
+var r = range_counted("f_i", "n_i");
 
+function half_nonnegative(n) {return n >> 1;}
+function twice(n) {return n + n;}
+
+function swap_ranges_n(f0, f1, n) {
+    while (n != 0) {
+        iter_swap(f0, f1);
+        f0 = successor(f0);
+        f1 = successor(f1);
+        --n;
+    }
+    return [f0, f1];
+}
+
+function copy_n(f_i, n, f_o) {
+    while (n != 0) {
+        sink(f_o, source(f_i));
+        f_i = successor(f_i);
+        f_o = successor(f_o);
+        --n;
+    }
+    return [f_i, f_o];
+}
+
+function reverse_copy(f_i, l_i, f_o) {
+    while ( ! equal(f_i, l_i)) {
+        l_i = predecessor(l_i);
+        sink(f_o, source(l_i));
+        f_o = successor(f_o);
+
+    } 
+    return f_o;
+}
+
+function reverse_n_with_buffer(f_i, n, f_b) {
+    return reverse_copy(f_b, copy_n(f_i, n, f_b)[1], f_i);
+}
+
+function reverse_n_adaptive(f_i, n_i, f_b, n_b) {
+    if (n_i < 2) return successor(f_i, n_i);
+    if (n_i <= n_b) return reverse_n_with_buffer(f_i, n_i, f_b);
+
+    var h_i = half_nonnegative(n_i);
+    var n_mod_2 = n_i - twice(h_i);
+    var m_i = successor(reverse_n_adaptive(f_i, h_i, f_b, n_b), n_mod_2);
+    var l_i = reverse_n_adaptive(m_i, h_i, f_b, n_b);
+
+    swap_ranges_n(f_i, m_i, h_i);
+    return l_i;
+
+}
+
+var s = add_sequence(random_array(16), "s");
+//var b = add_sequence(new Array(size(s)), "b");
+var b = add_sequence(new Array(4), "b");
+print(s);
+var r = reverse_n_adaptive(begin(s), size(s), begin(b), size(b));
+print(s);`
+
+
+
+,rotate_bidirectional:
+`var r0 = range_bounded("f", "m");
+var r1 = range_bounded("m", "l");
+
+function reverse(f, l) {
+    while (true) {
+        if (equal(f, l)) return;
+        l = predecessor(l);
+        if (equal(f, l)) return;
+        iter_swap(f, l);
+        f = successor(f);        
+    }
+}
+
+function rotate_bidirectional(f, m, l) {
+    reverse(f, m);
+    reverse(m, l);
+    reverse(f, l);
+}
+var s = add_sequence(random_array(), "s");
+print(s);
+rotate_bidirectional(begin(s), successor(begin(s), 3), end(s));
+print(s);
+print('...');`
 };
 
 function getSnippet(snippet) {
